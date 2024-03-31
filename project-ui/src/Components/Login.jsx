4456e29d9a1c9d516305cdc63fec5845f9345a30
Vadadoriya-Harshit <harshitpatelv9@gmail.com>
2024-03-31 21:47:05 +0530

    Update Login.jsx

--- project-ui/src/Components/Login.jsx
+++ project-ui/src/Components/Login.jsx
@@ -57,7 +57,8 @@ function Login() {
 
   return (
     <div className='container-fluid'>
-      <form className='login-container rounded rounded-4 row d-flex align-items-center' onSubmit={handleSubmit}>
+     <div className='rounded rounded-4'>
+     <form className='login-container  row d-flex align-items-center' onSubmit={handleSubmit}>
         <div className="col-12 col-md-6 m-auto bg-white rounded shadow p-0">
           <div className="login-info text-center py-4 d-flex justify-content-center align-items-center">
             <h1 className='fw-bold text-light'>
@@ -98,6 +99,7 @@ function Login() {
           </div>
         </div>
       </form>
+     </div>
     </div>
   )
 }
